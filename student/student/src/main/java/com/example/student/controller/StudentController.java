b0d968b21d35d3ce201f4f6567a7a4e67e5fcf74
MeharinChoudhury23040 <it23040@mbstu.ac.bd>
2026-01-22 23:50:31 +0600

    Add files via upload

---
+++ student/student/src/main/java/com/example/student/controller/StudentController.java
@@ -0,0 +1,38 @@
+package com.example.student.controller;
+
+import com.example.student.model.Student;
+import com.example.student.service.StudentService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+    private final StudentService service;
+
+    public StudentController(StudentService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return service.getAllStudents();
+    }
+
+    @PostMapping
+    public Student addStudent(@RequestBody Student student) {
+        return service.saveStudent(student);
+    }
+
+    @PutMapping("/{id}")
+    public Student updateStudent(@PathVariable Long id, @RequestBody Student student) {
+        return service.updateStudent(id, student);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteStudent(@PathVariable Long id) {
+        service.deleteStudent(id);
+    }
+}
